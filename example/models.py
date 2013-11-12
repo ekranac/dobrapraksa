c24b4f14d85962e0d601c5c2940146bc182822f5
ekranac <besal.ziga@gmail.com>
2013-11-12 17:54:01 +0100

    admin site & database

--- example/models.py
+++ example/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Example(models.Model):
     title=models.charField(max_length=20)
@@ -7,12 +8,8 @@ class Example(models.Model):
     date=models.DateTimeField(auto_now_add=True)
     approved=models.BooleanField(default=False)
 
-class User(models.Model):
-    first_name=models.CharField(max_length=100)
-    last_name=models.CharField(max_length=100)
-    email=models.CharField(max_length=30)
-
 class Comment(models.Model):
     author=models.ForeignKey(User)
     date=models.DateTimeField(auto_now_add=True)
     content=models.textField()
+    example=models.ForeignKey(Example)
