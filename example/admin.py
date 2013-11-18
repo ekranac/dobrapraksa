21c3e5c34626a3382ead754d7f659ea343575897
ekranac <besal.ziga@gmail.com>
2013-11-18 17:26:47 +0100

    admin site, imported example

---
+++ example/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from example.models import Example
+
+admin.site.register(Example)
--- example/models.py
+++ example/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Example(models.Model):
-    title=models.charField(max_length=20)
-    content=models.textField()
-    author=ForeignKey(User)
+    title=models.CharField(max_length=20)
+    content=models.TextField()
+    author=models.ForeignKey(User)
     date=models.DateTimeField(auto_now_add=True)
     approved=models.BooleanField(default=False)
 
 class Comment(models.Model):
     author=models.ForeignKey(User)
     date=models.DateTimeField(auto_now_add=True)
-    content=models.textField()
+    content=models.TextField()
     example=models.ForeignKey(Example)
