43f2727374997ebe9060bad1f0331e84900385c4
ekranac <besal.ziga@gmail.com>
2013-11-19 22:55:59 +0100

    example posts, url

--- dobrapraksa/settings.py
+++ dobrapraksa/settings.py
@@ -108,6 +108,7 @@ ROOT_URLCONF = 'dobrapraksa.urls'
 WSGI_APPLICATION = 'dobrapraksa.wsgi.application'
 
 TEMPLATE_DIRS = (
+    'C:/django/dobrapraksa/templates/'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
