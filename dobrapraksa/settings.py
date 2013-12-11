a2df37536f25b986af47e614b6e6169ee8382cfe
ekranac <besal.ziga@gmail.com>
2013-12-11 17:14:44 +0100

    userena mail settings

--- dobrapraksa/settings.py
+++ dobrapraksa/settings.py
@@ -173,7 +173,13 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'praksatest@gmail.com'
+EMAIL_HOST_PASSWORD = 'mizastol'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ANONYMOUS_USER_ID = -1
 
--- example/views.py
+++ example/views.py
@@ -3,12 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from example.forms import CreateCommentForm
 from example.models import Example, Comment
-<<<<<<< HEAD
 from django.http import HttpResponseRedirect
 from django.template.loader import get_template
-=======
 from django.contrib.auth.decorators import login_required
->>>>>>> 975f874b586fd5f7f3c02cdb0d5782b4ca8b84bc
 
 
 @login_required
